83aa341b5af437f2d15c24808fdba391421fe872
Rafael Perdigão <2221442@my.ipleiria.pt>
2024-06-24 14:08:37 +0100

    Reserva maybe done?

---
+++ src/main/java/Modelos/Reserva.java
@@ -0,0 +1,24 @@
+package Modelos;
+
+import java.time.LocalDate;
+
+public class Reserva {
+
+    private Obra obra;
+    private Socio socio;
+
+    public Reserva(Obra obra, Socio socio) {
+        this.obra = obra;
+        this.socio = socio;
+    }
+
+    public Obra getObra() {
+        return obra;
+    }
+
+    public Socio getSocio() {
+        return socio;
+    }
+
+
+}
